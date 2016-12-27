112411e785cff039603fdf48c428e1bb70455718
Christian Tietze <christian.tietze@gmail.com>
2016-12-27 09:19:23 +0100

    Initial Commit

---
+++ DrawingTest/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  DrawingTest
+//
+//  Created by Christian Tietze on 27/12/16.
+//  Copyright © 2016 Christian Tietze. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
